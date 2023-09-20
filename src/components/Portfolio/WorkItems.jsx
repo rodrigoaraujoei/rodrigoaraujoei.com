a2ebc1decd2723d86abe15a2dff5efd6c722aee6
Rodrigo Araujo <rodrigoaraujoei@gmail.com>
2023-09-20 12:07:33 +0100

    fixed some typos

--- src/components/Portfolio/WorkItems.jsx
+++ src/components/Portfolio/WorkItems.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from '@iconify/react';
 
 const WorkItems = ({ item }) => {
   return (
@@ -6,27 +7,27 @@ const WorkItems = ({ item }) => {
       <img src={item.image} alt="" className="work__img" />
 
       <h3 className="work__title">{item.title}</h3>
-
-      <div className="work__buttons">
-        <a
-          href={item.repo}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="work__button"
-        >
-          GitHub
-        </a>
-
-        <a
-          href={item.live}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="work__button"
-        >
-          Live Demo
-        </a>
         
-      </div>
+
+      <a
+        href={item.live}
+        target="_blank"
+        className="work__button "
+      >
+       
+       <i className="services__modal-icon"><Icon icon="uil-caret-right" /></i> Live
+      </a>
+
+      
+      <a
+        href={item.repo}
+        target="_blank"
+        className="work__button "
+      >
+       
+       <i className="services__modal-icon"><Icon icon="uil-arrow" /></i> Code
+      </a>
+
       
     </div>
   );
