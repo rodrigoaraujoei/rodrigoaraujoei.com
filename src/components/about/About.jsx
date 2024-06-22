f9395cc6132baf55ff772566dfb0b067e07bcaa7
Rodrigo Araujo <rodrigoaraujoei@gmail.com>
2024-06-22 12:50:08 +0100

    new photo

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import AboutImg from "../../assets/estacao.jpg";
+import AboutImg from "../../assets/eubw.jpg";
 import CV from "../../assets/RodrigoAraujoCV.pdf";
 import Info from "./Info";
 
