69f34dc38c17904d985ead7495ec3828f68a9114
Rodrigo Araujo <rodrigoaraujoei@gmail.com>
2024-06-22 12:43:13 +0100

    fixed links

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/estacao.jpg";
-import CV from "../../assets/RodrigoAraujo_CV.pdf";
+import CV from "../../assets/RodrigoAraujoCV.pdf";
 import Info from "./Info";
 
 const About = () => {
